f0984e0324eba14680a7e9b21eefe8033b4110a9
khanhnguyen <khanhnguyen99hn@gmail.com>
2019-04-13 14:50:04 -0400

    update

--- content.js
+++ content.js
@@ -1,11 +1,14 @@
 // content.js
 
-window.onload = function() {
-	var node = document.getElementById('buyNow')
-  node.parentElement.removeChild(node)
-
+if(document.getElementById('buyNow') != null) {
+  document.getElementById('buyNow').onclick = function() {
+    var node = document.getElementById('buyNow')
+    node.parentElement.removeChild(node)
+  }
 }
 
-document.getElementById("addToCart_feature_div").onclick = function () {
-      alert("Are you sure?");
-};
+if(document.getElementById('addToCart_feature_div') != null) {
+  document.getElementById('addToCart_feature_div').onclick = function() {
+    return confirm('are you sure?');
+  }
+}
