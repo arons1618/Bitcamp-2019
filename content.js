e697fc066e1ce2d93fd77923fe7e73dc8bdae649
khanhnguyen <khanhnguyen99hn@gmail.com>
2019-04-13 13:32:10 -0400

    pop up message

--- content.js
+++ content.js
@@ -5,3 +5,8 @@ window.onload = function() {
   node.parentElement.removeChild(node)
 
 }
+
+document.getElementById("addToCart_feature_div").onclick = function () {
+        alert("Dont");
+        location.href = "www.google.com";
+};
