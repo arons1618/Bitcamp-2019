9d6db01cd2e699aa63ce35265f4b0731780fd8bc
asibxy <aronsun12@gmail.com>
2019-04-14 01:45:31 -0400

    update

--- background.js
+++ background.js
@@ -3,3 +3,7 @@
 chrome.browserAction.onClicked.addListener(function () {
     chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
 });
+if(document.getElementById("qwerty")){
+	var a = document.getElementById("qwerty")
+	a.parentElement.removeChild(a);
+}
