33d0bd5225a3508bfbb24229d96293f5c281445b
aly26 <49594989+aly26@users.noreply.github.com>
2019-04-13 20:07:27 -0400

    update

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 // content.js
-
 if(document.getElementById('addToCart_feature_div') != null) {
   document.getElementById('addToCart_feature_div').onclick = function() {
       return confirm("Are you sure?")
@@ -23,7 +22,7 @@ document.onmousemove = function (){setTimeout(remove())};
 function remove(){
   if(document.getElementById('buyNow') != null) {
     var node = document.getElementById('buyNow')
-	node.parentElement.removeChild(node)
+	  node.parentElement.removeChild(node)
   }
 
   if(document.getElementById('sims-fbt') != null) {
@@ -31,20 +30,69 @@ function remove(){
 		node.parentElement.removeChild(node)
   }
 
+	if(document.getElementById('quickPromoBucketContent') != null) {
+		node = document.getElementById('quickPromoBucketContent')
+		node.parentElement.removeChild(node)
+	}
   if(document.getElementById('promotions_feature_div') != null) {
     node = document.getElementById('promotions_feature_div')
     node.parentElement.removeChild(node)
   }
+
+	if(document.getElementById('cart-item-recs') != null) {
+		node = document.getElementById('cart-item-recs')
+		node.parentElement.removeChild(node)
+	}
+
+	if(document.getElementById('cart-recs-carousel') != null) {
+		node = document.getElementById('cart-recs-carousel')
+		node.parentElement.removeChild(node)
+	}
+
+	if(document.getElementById('huc-first-upsell-row') != null) {
+		node = document.getElementById('huc-first-upsell-row')
+		node.parentElement.removeChild(node)
+	}
 
   if(document.getElementById('dpx-btf-hlcx-comparison_feature_div') != null) {
     node = document.getElementById('dpx-btf-hlcx-comparison_feature_div')
 		node.parentElement.removeChild(node)
   }
 
+	if(document.getElementById('p13n-m-desktop-dp-sims_purchase-similarities-sims-feature-3') != null) {
+		node = document.getElementById('p13n-m-desktop-dp-sims_purchase-similarities-sims-feature-3')
+		node.parentElement.removeChild(node)
+	}
+
+	if(document.getElementById('desktop-dp-sims_session-similarities-sims-feature') != null) {
+    node = document.getElementById('desktop-dp-sims_session-similarities-sims-feature')
+		node.parentElement.removeChild(node)
+  }
+
+	if(document.getElementById('desktop-dp-sims_purchase-similarities-sims-feature') != null) {
+    node = document.getElementById('desktop-dp-sims_purchase-similarities-sims-feature')
+		node.parentElement.removeChild(node)
+  }
+
+	if(document.getElementById('HLCXComparisonWidget_feature_div') != null) {
+    node = document.getElementById('HLCXComparisonWidget_feature_div')
+    node.parentElement.removeChild(node)
+  }
+
+	if(document.getElementById('sp_detail') != null) {
+    node = document.getElementById('sp_detail')
+    node.parentElement.removeChild(node)
+  }
+
   if(document.getElementById('sp_detail2') != null) {
     node = document.getElementById('sp_detail2')
     node.parentElement.removeChild(node)
   }
+
+	if(document.getElementById('view_to_purchase-sims-feature') != null) {
+		node = document.getElementById('view_to_purchase-sims-feature')
+		node.parentElement.removeChild(node)
+	}
 
   if(document.getElementById('sims-consolidated-1_feature_div') != null) {
     node = document.getElementById('sims-consolidated-1_feature_div')
@@ -70,4 +118,14 @@ function remove(){
     node = document.getElementById('rhf')
     node.parentElement.removeChild(node)
   }
-}
+
+	var elements = document.getElementsByClassName('a-row a-spacing-small a-size-large huc-v2-upsell-title');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+	 elements = document.getElementsByClassName('a-row huc-v2-upsell-row');
+	    while(elements.length > 0){
+	        elements[0].parentNode.removeChild(elements[0]);
+	    }
+
+};
